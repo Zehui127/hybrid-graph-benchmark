e02beab22a12868a57e0da96b43b849777745bfe
Zehui127 <zl432@cantab.ac.uk>
2023-04-21 13:48:19 +0100

    fix regression for sage model

--- hybrid_graph/models/gnn/baseline.py
+++ hybrid_graph/models/gnn/baseline.py
@@ -10,24 +10,37 @@ class GCNNet(torch.nn.Module):
             self, info, *args, **kwargs):
         super().__init__()
         self.conv1 = GCNConv(info["num_node_features"], 16)
-        self.conv2 = GCNConv(16, info["num_classes"])
-
+        self.is_regression = info["is_regression"]
+        if info["is_regression"]:
+            self.conv2 = GCNConv(16, 16)
+            self.head = nn.Linear(16, 1)
+        else:
+            self.conv2 = GCNConv(16, info["num_classes"])
     def forward(self, data, *args, **kargs):
         x, edge_index = data.x, data.edge_index
         x = F.relu(self.conv1(x, edge_index))
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
-        return F.log_softmax(x, dim=1)
+        if self.is_regression:
+            x = self.head(x).squeeze()
+        else:
+            x = F.log_softmax(x, dim=1)
+        return x
 
 
 class SAGENet(torch.nn.Module):
     def __init__(
             self, info, mixture_cls=None, load_config=None, *args, **kwargs):
         super(SAGENet, self).__init__()
+        self.is_regression = info["is_regression"]
+        if self.is_regression:
+            self.conv2 = SAGEConv(16, 16, normalize=False)
+            self.head = nn.Linear(16, 1)
+        else:
+            self.conv2 = SAGEConv(16, info["num_classes"], normalize=False)
+
         self.conv1 = SAGEConv(
             info["num_node_features"], 16, normalize=False)
-        self.conv2 = SAGEConv(
-            16, info["num_classes"], normalize=False)
 
     def forward(self, data, *args, **kargs):
         x, edge_index = data.x, data.edge_index
@@ -35,4 +48,8 @@ class SAGENet(torch.nn.Module):
             self.conv1(x, edge_index))
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.conv2(x, edge_index)
-        return F.log_softmax(x, dim=1)
+        if self.is_regression:
+            x = self.head(x).squeeze()
+        else:
+            x = F.log_softmax(x, dim=1)
+        return x
